555def29a3667f7571c85f8e5307fca6684ed434
Igor Bessonov <ikogep23@gmail.com>
2024-08-18 11:18:28 +0300

    small fix of prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,7 +8,7 @@ def is_prime(n: int):
     '''Check if given number is prime or not.'''
     if n == 1:
         return False
-    for i in range(2, int(n * 0.5) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     else:
